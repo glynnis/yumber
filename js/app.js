3d051212ed5c94a2d7e8ff029e65c5ce768af668
Glynnis Ritchie and Chris Wilson <dev+glynnis+twopoint718@bendyworks.com>
2014-07-24 11:38:15 -0500

    Add view for individual menu

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.resource('menu');
   this.resource('cart');
+  this.resource('menus');
+  this.resource('menu', { path: '/:id' });
 });
 
 App.MenuRoute = Ember.Route.extend({
+  model: function(params) {
+    return menus.findBy('id', parseInt(params.id));
+  }
+});
+
+App.MenusRoute = Ember.Route.extend({
   model: function() {
-    return ['breakfast', 'lunch', 'dinner'];
+    return menus;
   }
 });
 
@@ -19,12 +26,12 @@ App.CartRoute = Ember.Route.extend({
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return restaurants;
+    return menus;
   }
 });
 
-var restaurants = [
-  { id: 1, name: "Joe's"},
-  { id: 2, name: "Graze"},
-  { id: 3, name: "Taco Bell"},
+var menus = [
+  { id: 1, name: "Joe's", items: ["Eggs", "Bacon", "Biscuit"] },
+  { id: 2, name: "Graze", items: ["Cheese curds", "Beet burger", "Soup"] },
+  { id: 3, name: "Taco Bell", items: ["Sloppy taco", "Gross burrito", "Fried thing"] },
 ];
